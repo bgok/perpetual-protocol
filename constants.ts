e5c5c7528234344ffbe107efcac496f945d1d611
bgok <ken@citizenhodler.com>
2021-12-22 22:24:11 -0800

    Use a test token contract

--- constants.ts
+++ constants.ts
@@ -15,7 +15,6 @@ export const HOMESTEAD_URL = `${process.env["WEB3_HOMESTEAD_ENDPOINT"]}`
 export const SOKOL_URL = `${process.env["WEB3_SOKOL_ENDPOINT"]}`
 export const XDAI_URL = `${process.env["WEB3_XDAI_ENDPOINT"]}`
 // FIXME Get from an environmental variable
-export const TBSC_URL = "https://data-seed-prebsc-1-s1.binance.org:8545/"
 export const RINKEBY_ARCHIVE_NODE_URL = `${process.env["ALCHEMY_RINKEBY_ENDPOINT"]}`
 export const HOMESTEAD_ARCHIVE_NODE_URL = `${process.env["ALCHEMY_HOMESTEAD_ENDPOINT"]}`
 export const XDAI_ARCHIVE_NODE_URL = "https://xdai-archive.blockscout.com"
@@ -26,11 +25,12 @@ export const HOMESTEAD_MNEMONIC = process.env["HOMESTEAD_MNEMONIC"] || ""
 export const SOKOL_MNEMONIC = process.env["SOKOL_MNEMONIC"] || ""
 export const XDAI_MNEMONIC = process.env["XDAI_MNEMONIC"] || ""
 // FIXME Get from an environmental variable
-export const TBSC_MNEMONIC = "<wallet words go here>"
+export const TBSC_URL = "https://data-seed-prebsc-1-s1.binance.org:8545/"
+export const TBSC_MNEMONIC = "exchange employ enroll misery across pretty asthma bone air since together venue"
+export const TBSC_GAS_PRICE = 20_000_000_000
 export const ARTIFACTS_DIR = "./build/contracts"
 export const GAS = 8000000
 export const GAS_PRICE = 2_000_000_000
-export const TBSC_GAS_PRICE = 20_000_000_000
 export const SRC_DIR = join(ROOT_DIR, SRC_DIR_NAME)
 export const LEGACY_SRC_DIR = join(ROOT_DIR, LEGACY_SRC_DIR_NAME)
 export const ETHERSCAN_API_KEY = process.env["ETHERSCAN_API_KEY"] || ""
