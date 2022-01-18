99d0443fa60ab3f19512eb11fed935bb0906d538
bgok <ken@citizenhodler.com>
2022-01-18 11:05:31 -0800

    Use a .env file in the root for private keys.

--- constants.ts
+++ constants.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv"
-import { join, resolve } from "path"
+import { join } from "path"
 
-dotenv.config({ path: resolve(__dirname, "..", "..", ".env") })
+dotenv.config()
 
 export const ROOT_DIR = __dirname
 export const SRC_DIR_NAME = "src"
@@ -15,7 +15,6 @@ export const RINKEBY_URL = `${process.env["WEB3_RINKEBY_ENDPOINT"]}`
 export const HOMESTEAD_URL = `${process.env["WEB3_HOMESTEAD_ENDPOINT"]}`
 export const SOKOL_URL = `${process.env["WEB3_SOKOL_ENDPOINT"]}`
 export const XDAI_URL = `${process.env["WEB3_XDAI_ENDPOINT"]}`
-// FIXME Get from an environmental variable
 export const RINKEBY_ARCHIVE_NODE_URL = `${process.env["ALCHEMY_RINKEBY_ENDPOINT"]}`
 export const HOMESTEAD_ARCHIVE_NODE_URL = `${process.env["ALCHEMY_HOMESTEAD_ENDPOINT"]}`
 export const XDAI_ARCHIVE_NODE_URL = "https://xdai-archive.blockscout.com"
@@ -25,9 +24,8 @@ export const RINKEBY_MNEMONIC = process.env["RINKEBY_MNEMONIC"] || ""
 export const HOMESTEAD_MNEMONIC = process.env["HOMESTEAD_MNEMONIC"] || ""
 export const SOKOL_MNEMONIC = process.env["SOKOL_MNEMONIC"] || ""
 export const XDAI_MNEMONIC = process.env["XDAI_MNEMONIC"] || ""
-// FIXME Get from an environmental variable
-export const TBSC_URL = "https://data-seed-prebsc-1-s1.binance.org:8545/"
-export const TBSC_MNEMONIC = "exchange employ enroll misery across pretty asthma bone air since together venue"
+export const TBSC_URL = process.env["TBSC_URL"] || ""
+export const TBSC_MNEMONIC = process.env["TBSC_MNEMONIC"] || ""
 export const TBSC_GAS_PRICE = 20_000_000_000
 export const ARTIFACTS_DIR = "./build/contracts"
 export const GAS = 8_000_000
